04d16672053bd2e11e7d25c967d8f7a097ca98c9
Ryo Yokoyama <yokkohsan@gmail.com>
2019-06-16 23:26:55 +0900

    Modified binary format.

--- dataset/README.md
+++ dataset/README.md
@@ -12,12 +12,13 @@ CAD|-       |-       |-       |-       |-
 
 ## Binary Format
 
-Each tick data consists of 22 bytes.
+Each tick data consists of 24 bytes.
 
-/    |UNIX(sec)|UNIX(ms)|ASK|BID|ASK vol|BID vol
------|---------|--------|---|---|-------|-------
-bytes|4        |2       |4  |4  |4      |4
+/    |UNIX|ASK|BID|ASK-vol|BID-vol
+-----|----|---|---|-------|-------
+bytes|6   |4  |4  |5      |5
 
 * Little endian
 * Timezone is UTC
-* ASK/BID should be devide by 1,000,000
+* UNIX timestamp needs to be divided by 1,000
+* ASK/BID needs to be divided by 1,000,000
--- dataset/binarizer/main.py
+++ dataset/binarizer/main.py
@@ -31,28 +31,25 @@ with open(path_in,'r') as src:
             t = datetime.strptime(datas[0], '%Y-%m-%d %H:%M:%S.%f %z').timestamp()
             
             # raw data
-            t_unix = int(t)
-            t_ms = round((t - t_unix) * 1000)
+            unix = int(t * 1000)
             ask = int(float(datas[1]) * norm_val)
-            bid = int(float(datas[2]) * norm_val)
+            bid = int(float(datas[2]) * norm_val)                            
             ask_vol = int(datas[3])
             bid_vol = int(datas[4])
             
             # binary data
-            t_unix_b = t_unix.to_bytes(4, endian)
-            t_ms_b = t_ms.to_bytes(2, endian)
+            unix_b = unix.to_bytes(6, endian)
             ask_b = ask.to_bytes(4, endian)
             bid_b = bid.to_bytes(4, endian)
-            ask_vol_b = ask_vol.to_bytes(4, endian)
-            bid_vol_b = bid_vol.to_bytes(4, endian)
+            ask_vol_b = ask_vol.to_bytes(5, endian)
+            bid_vol_b = bid_vol.to_bytes(5, endian)            
             
             if debug:
                 print(datas)
-                print(t_unix, t_ms, ask, bid, ask_vol, bid_vol)
-                print(t_unix_b.hex(), t_ms_b.hex(), ask_b.hex(), bid_b.hex(), ask_vol_b.hex(), bid_vol_b.hex())
+                print(unix, ask, bid, ask_vol, bid_vol)
+                print(unix_b.hex(), ask_b.hex(), bid_b.hex(), ask_vol_b.hex(), bid_vol_b.hex())
             
-            dst.write(t_unix_b)
-            dst.write(t_ms_b)
+            dst.write(unix_b)
             dst.write(ask_b)
             dst.write(bid_b)
             dst.write(ask_vol_b)
